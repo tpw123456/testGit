9d79be05ee9ca5575afc05f457519e2a72d4de02
Administrator <Administrator@SC-201908100955>
2019-11-12 11:27:30 +0800

    testGit

--- test/testgitup/src/main/java/com/szht/TestgitupApplication.java
+++ test/testgitup/src/main/java/com/szht/TestgitupApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestgitupApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestgitupApplication.class, args);
 	}
