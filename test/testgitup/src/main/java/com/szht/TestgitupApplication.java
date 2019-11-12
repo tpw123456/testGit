b35631b4dc5c47de5b62eb599abb8898e60dbc9b
Administrator <Administrator@SC-201908100955>
2019-11-12 11:14:27 +0800

    testGitup

---
+++ test/testgitup/src/main/java/com/szht/TestgitupApplication.java
@@ -0,0 +1,13 @@
+package com.szht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgitupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestgitupApplication.class, args);
+	}
+
+}
---
+++ test/testgitup/src/test/java/com/szht/TestgitupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
